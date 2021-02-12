f1d2905f8bad6ac3b7273800e9e7b3c82c06f2a4
Farid <faridggl100@gmail.com>
2021-02-12 15:39:34 +0200

    remove unused ports and restart:always for java_client and modifing java code to delay 1 minute so that postgres service starts

--- java_client/Main.java
+++ java_client/Main.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.sql.*;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
@@ -8,12 +9,18 @@ public class Main {
     private static String email;
 
     public static void main(String[] args) {
+
+        System.out.println("\n\nInserting new User to Postgres============>>>>>\n\n");
+
         try {
+            // waits one minute to postgres server to come up
+            TimeUnit.MINUTES.sleep(1);
+
             File file = new File("input_data.txt");
             Scanner scan = new Scanner(file);
 
 
-
+            // read whatever line exists in the file and store it in postgres
             while(scan.hasNext()){
                 String data[] = scan.nextLine().trim().split(" ");
                 name = data[0];
@@ -38,7 +45,7 @@ public class Main {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
-        System.out.println("Opened database successfully");
+        System.out.println("inserted ("+ name + " " + email +")successfully");
     }
 
 }
